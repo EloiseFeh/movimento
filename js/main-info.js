b70cbfb9c9c152f88a65bdab546ae9102c617a4a
rafaellyff <rafa1045.rf@gmail.com>
2020-04-29 00:02:57 -0300

    Faz o scroll do objeto

--- js/main-info.js
+++ js/main-info.js
@@ -2,30 +2,6 @@
  * @author Container Digital Jr <containerdigitaljr@gmail.com>
  */
 
-// var window_width = $(window).width() - $('#object').width();
-
-// var document_height = $(document).height() - $(window).height();
-
-// $(function () {
-//     $(window).scroll(function () {
-//         var scroll_position = $(window).scrollTop();
-//         var object_position_left = window_width * (scroll_position / document_height);
-//         $('#object').css({
-//             'left': object_position_left
-//         });
-//     });
-// });
-
-window.addEventListener('scroll', () => {
-	console.log('scroll');
-	var window_width = $(window).width() - $('#object').width();
-
-	 var document_height = $(document).height() - $(window).height();
-
-	 var scroll_position = $(window).scrollTop();
-        var object_position_left = window_width * (scroll_position / document_height);
-        $('#object').css({
-            'left': object_position_left
-        });
-
-});
+function movimentacao() {
+  console.log($(".wrapper").offset().left * -1);
+}
