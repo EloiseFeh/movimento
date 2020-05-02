3fd109eaae3392ecf4aa2c4901c9f4eef174c518
Ana Eloise <eloise@diogosoares.com.br>
2020-05-02 10:35:12 -0300

    alterações 01/05

--- js/main-info.js
+++ js/main-info.js
@@ -18,60 +18,81 @@ window.addEventListener('scroll', () => {
             'left': object_position_left
         });
 
-        window.addEventListener('keyup', (e) => {
-    	switch (e.key) {
-        case 'ArrowLeft':
-            circle.style.left = object_position_left;
-            break;
-        case 'ArrowRight':
-            circle.style.left = object_position_left;
-            break;
-    }
 });
 
-});
+function trocar1() {
+    var imagem=document.getElementById("a");
+    if(imagem.alt == "out1"){
+        imagem.src = "imgs/outdoor12.png";
+        imagem.alt = "out2";
+    }else if (imagem.alt=="out2") {
+        imagem.src = "imgs/outdoor13.png";
+        imagem.alt = "out3";
+    }
+    else {
+        imagem.src = "imgs/outdoor11.png";
+        imagem.alt = "out1";
+    }
+}
+
+function trocar2() {
+    var imagem=document.getElementById("b");
+    if(imagem.alt == "out21"){
+        imagem.src = "imgs/outdoor22.png";
+        imagem.alt = "out22";
+    }else if (imagem.alt=="out22") {
+        imagem.src = "imgs/outdoor23.png";
+        imagem.alt = "out23";
+    }else if (imagem.alt=="out23") {
+        imagem.src = "imgs/outdoor24.png";
+        imagem.alt = "out24";
+    }
+    else {
+        imagem.src = "imgs/outdoor21.png";
+        imagem.alt = "out21";
+    }
+}
 
 function movimentacao() {
-	var scroll = $(".wrapper").offset().left*-1,
-		target = $(".anime"),
-		animationClass = 'anime-start',
-		offset = $(window).width()*26/4;
-  console.log("auau "+scroll);
+	var scroll = $(".wrapper").offset().left*-1;
+    target = $(".anime"),
+    target2 = $(".anime2"),
+    target3 = $(".anime3"),
+    target4 = $(".anime4"),
+    target5 = $(".anime5"),
+    target6 = $(".anime6"),
+    target7 = $(".anime7"),
+    placa = $(".anime8"),
+		animationClass = 'anime-start';
+  //console.log("auau "+scroll);
   //console.log(target);
   //console.log(animationClass);
-  function animeScroll(){
-  target.each(function(){
+ 
+  function animeScroll(classe, numerador, denominador){
+         var offset = $(window).width()*numerador/denominador;
+  classe.each(function(){
   	var itemLeft = $(this).offset().left;
   	console.log(itemLeft);
-  	if (scroll-offset > itemLeft) {
+      if (scroll-offset > itemLeft) {
   		$(this).addClass(animationClass);
-  	}else{
+  	}
+    else{
   		$(this).removeClass(animationClass);
   	}
-
   })
 }
-animeScroll();
+
+animeScroll(target,11.5,4);
+animeScroll(target2,16,4);
+animeScroll(target3,19,4);
+animeScroll(target4,28,4);
+animeScroll(target5,22,4);
+animeScroll(target6,30,4);
+animeScroll(target7,32,4);
+animeScroll(placa,10,4);
 
 }
 
-let circle = document.querySelector('#object');
-let moveBy = 10;
- 
-window.addEventListener('load', () => {
-    circle.style.position = 'absolute';
-    circle.style.left = 0;
-    circle.style.top = 0;
-});
 
-window.addEventListener('keyup', (e) => {
-    switch (e.key) {
-        case 'ArrowLeft':
-            circle.style.left = parseInt(circle.style.left) - moveBy + 'px';
-            break;
-        case 'ArrowRight':
-            circle.style.left = parseInt(circle.style.left) + moveBy + 'px';
-            break;
-    }
-});
+
 
