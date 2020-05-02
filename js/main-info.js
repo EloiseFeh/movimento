a08ebdbbc77863cb4c5a9c775382ebcb01630608
Ana Eloise <eloise@diogosoares.com.br>
2020-05-02 10:36:53 -0300

    resolvendo uns BO

--- js/main-info.js
+++ js/main-info.js
@@ -54,24 +54,3 @@ function movimentacao() {
 animeScroll();
 
 }
-
-let circle = document.querySelector('#object');
-let moveBy = 10;
- 
-window.addEventListener('load', () => {
-    circle.style.position = 'absolute';
-    circle.style.left = 0;
-    circle.style.top = 0;
-});
-
-window.addEventListener('keyup', (e) => {
-    switch (e.key) {
-        case 'ArrowLeft':
-            circle.style.left = parseInt(circle.style.left) - moveBy + 'px';
-            break;
-        case 'ArrowRight':
-            circle.style.left = parseInt(circle.style.left) + moveBy + 'px';
-            break;
-    }
-});
-
